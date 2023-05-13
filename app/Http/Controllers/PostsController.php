6e937729a0a77625efaa62e9059fc0aacf95532d
Harsh Lilawala <lilawalaharsh@gmail.com>
2023-05-13 14:06:06 +0100

    trying of show

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,75 +1,80 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\Post;
+
 use Illuminate\Http\Request;
+use App\Models\Post;
+use Cviebrock\EloquentSluggable\Services\SlugService;
 
 class PostsController extends Controller
 {
+ 
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
         return view('blog.index')
-        ->with('posts',Post::orderBy('updated_at','DESC')->get());
+            ->with('posts', Post::orderBy('updated_at','DESC')->get());
     }
 
     /**
      * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
         return view('blog.create');
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
         $request->validate([
-            'title'=>'required',
-            'description'=>'required',
-            'image'=>'required|mimes:jpg,jpeg,png|max:5048'
+            'title' => 'required',
+            'description' => 'required',
+            'image' => 'required|mimes:jpg,png,jpeg|max:5048'
         ]);
+
         $newImageName = uniqid() . '-' . $request->title . '.' . $request->image->extension();
 
         $request->image->move(public_path('images'), $newImageName);
- 
-       dd($newImageName);
+
+        Post::create([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'more' => SlugService::createSlug(Post::class, 'more', $request->title),
+            'image_path' => $newImageName,
+            'user_id' => auth()->user()->id
+        ]);
+
+        return redirect('/blog')
+        ->with('message', 'Your post has been added!');
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param  string  $slug
+     * @return \Illuminate\Http\Response
      */
-    public function show(string $id)
+    public function show($more)
     {
-        //
+        return view('blog.show')
+        ->with('post', Post::where('more', $more)->first());
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
+}
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
-} 
